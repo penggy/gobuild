0fd67ddfb8099a859f7562b8a3e7770e5dd39e70
penggy <peng.wu@foxmail.com>
2019-01-02 19:14:01 +0800

    reset to dc82ceeff15d9da85239c004b7b3686e223645df

--- gobuild.go
+++ gobuild.go
@@ -2,96 +2,88 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// 一个简单的 Go 语言热编译工具。
-//
-// 监视指定目录(可同时监视多个目录)下文件的变化，触发`go build`指令，
-// 实时编译指定的 Go 代码，并在编译成功时运行该程序。
-// 具体命令格式可使用`gobuild -h`来查看。
-package main // import "github.com/caixw/gobuild"
+package gobuild // import "github.com/caixw/gobuild"
 
 import (
-	"flag"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
-
-	"github.com/issue9/term/colors"
 )
 
-// 当前程序的主要版本号
-const mainVersion = "0.6.5"
-
-// 与版号相关的变量
-var (
-	buildDate  string // 由链接器提供此值。
-	commitHash string // 由链接器提供此值。
-	version    string
-)
-
-func init() {
-	version = mainVersion
-	if len(buildDate) > 0 {
-		version += ("+" + buildDate)
+// Build 执行热编译服务。
+//
+// logs 日志输出通道
+// mainFiles 为 go build 最后的文件参数，可以为空，表示当前目录；
+// outputName 指定可执行文件输出的文件路径，为空表示默认值；
+// exts 指定监视的文件扩展名，为空表示不监视任何文件，* 表示监视所有文件；
+// recursive 是否监视子目录；
+// appArgs 传递给程序的参数；
+// dir 表示需要监视的目录，至少指定一个目录，第一个目录被当作子目录，将编译其下的文件。
+func Build(logs chan *Log, mainFiles, outputName, exts string, recursive bool, appArgs string, dir ...string) error {
+	if len(dir) < 1 {
+		return errors.New("参数 dir 至少指定一个")
 	}
-}
-
-func main() {
-	var showHelp, showVersion, recursive, showIgnoreLog bool
-	var mainFiles, outputName, extString, appArgs string
-
-	flag.BoolVar(&showHelp, "h", false, "显示帮助信息；")
-	flag.BoolVar(&showVersion, "v", false, "显示版本号；")
-	flag.BoolVar(&recursive, "r", true, "是否查找子目录；")
-	flag.BoolVar(&showIgnoreLog, "i", false, "是否显示被标记为 IGNORE 的日志内容；")
-	flag.StringVar(&outputName, "o", "", "指定输出名称，程序的工作目录随之改变；")
-	flag.StringVar(&appArgs, "x", "", "传递给编译程序的参数；")
-	flag.StringVar(&extString, "ext", "go", "指定监视的文件扩展，区分大小写。* 表示监视所有类型文件，空值代表不监视任何文件；")
-	flag.StringVar(&mainFiles, "main", "", "指定需要编译的文件；")
-	flag.Usage = usage
-	flag.Parse()
-
-	switch {
-	case showHelp:
-		flag.Usage()
-		return
-	case showVersion:
-		fmt.Fprintln(os.Stdout, "gobuild", version, "build with", runtime.Version(), runtime.GOOS+"/"+runtime.GOARCH)
-
-		if len(commitHash) > 0 {
-			fmt.Fprintln(os.Stdout, "commitHash:", commitHash)
-
-		}
-		return
-	case showIgnoreLog:
-		ignore = log.New(&logWriter{out: os.Stderr, color: colors.Default, prefix: "[IGNO]"}, "", log.Ltime)
-	}
-
-	wd, err := os.Getwd()
+	wd, err := filepath.Abs(dir[0])
 	if err != nil {
-		erro.Println("获取当前工作目录时，发生以下错误:", err)
-		return
+		return err
+	}
+	dir[0] = wd
+
+	appName, err := getAppName(outputName, wd)
+	if err != nil {
+		return err
 	}
 
 	// 初始化 goCmd 的参数
-	args := []string{"build", "-o", outputName}
+	args := []string{"build", "-o", appName}
 	if len(mainFiles) > 0 {
 		args = append(args, mainFiles)
 	}
 
 	b := &builder{
-		exts:      getExts(extString),
-		appName:   getAppName(outputName, wd),
+		exts:      getExts(exts),
+		appName:   appName,
 		appArgs:   splitArgs(appArgs),
 		goCmdArgs: args,
+		logs:      logs,
 	}
 
-	w, err := b.initWatcher(recursivePaths(recursive, append(flag.Args(), wd)))
+	// 输出提示信息
+	logs <- &Log{
+		Type:    LogTypeInfo,
+		Message: fmt.Sprint("给程序传递了以下参数：", b.appArgs),
+	}
+
+	// 提示扩展名
+	switch {
+	case len(b.exts) == 0: // 允许不监视任意文件，但输出一信息来警告
+		logs <- &Log{
+			Type:    LogTypeWarn,
+			Message: "将 ext 设置为空值，意味着不监视任何文件的改变！",
+		}
+	case len(b.exts) > 0:
+		logs <- &Log{
+			Type:    LogTypeInfo,
+			Message: fmt.Sprint("系统将监视以下类型的文件:", b.exts),
+		}
+	}
+
+	// 提示 appName
+	logs <- &Log{
+		Type:    LogTypeInfo,
+		Message: fmt.Sprint("输出文件为:", b.appName),
+	}
+
+	paths, err := recursivePaths(recursive, dir)
 	if err != nil {
-		erro.Println(err)
-		return
+		return err
+	}
+	w, err := b.initWatcher(paths)
+	if err != nil {
+		return err
 	}
 	defer w.Close()
 
@@ -99,6 +91,7 @@ func main() {
 	go b.build()
 
 	<-make(chan bool)
+	return nil
 }
 
 func splitArgs(args string) []string {
@@ -133,55 +126,20 @@ func splitArgs(args string) []string {
 		ret = append(ret, args[start:len(args)])
 	}
 
-	info.Println("给程序传递了以下参数：", ret)
-
 	return ret
 }
 
-func usage() {
-	fmt.Fprintln(os.Stdout, `gobuild 是 Go 的热编译工具，监视文件变化，并编译和运行程序。
-
-命令行语法：
- gobuild [options] [dependents]
-
- options:`)
-
-	flag.CommandLine.SetOutput(os.Stdout)
-	flag.PrintDefaults()
-
-	fmt.Fprintln(os.Stdout, `
- dependents:
-  指定其它依赖的目录，只能出现在命令的尾部。
-
-
-常见用法:
-
- gobuild
-   监视当前目录，若有变动，则重新编译当前目录下的 *.go 文件；
-
- gobuild -main=main.go
-   监视当前目录，若有变动，则重新编译当前目录下的 main.go 文件；
-
- gobuild -main="main.go" dir1 dir2
-   监视当前目录及 dir1 和 dir2，若有变动，则重新编译当前目录下的 main.go 文件；
-
-
-NOTE: 不会监视隐藏文件和隐藏目录下的文件。
-
-源代码采用 MIT 开源许可证，并发布于 https://github.com/caixw/gobuild`)
-}
-
 // 根据 recursive 值确定是否递归查找 paths 每个目录下的子目录。
-func recursivePaths(recursive bool, paths []string) []string {
+func recursivePaths(recursive bool, paths []string) ([]string, error) {
 	if !recursive {
-		return paths
+		return paths, nil
 	}
 
 	ret := []string{}
 
 	walk := func(path string, fi os.FileInfo, err error) error {
 		if err != nil {
-			erro.Println("在遍历监视目录时，发生以下错误:", err)
+			return err
 		}
 
 		if fi.IsDir() && strings.Index(path, "/.") < 0 {
@@ -192,11 +150,11 @@ func recursivePaths(recursive bool, paths []string) []string {
 
 	for _, path := range paths {
 		if err := filepath.Walk(path, walk); err != nil {
-			erro.Println("在遍历监视目录时，发生以下错误:", err)
+			return nil, err
 		}
 	}
 
-	return ret
+	return ret, nil
 }
 
 // 将 extString 分解成数组，并清理掉无用的内容，比如空字符串
@@ -216,34 +174,27 @@ func getExts(extString string) []string {
 		ret = append(ret, ext)
 	}
 
-	switch {
-	case len(ret) == 0: // 允许不监视任意文件，但输出一信息来警告
-		warn.Println("将 ext 设置为空值，意味着不监视任何文件的改变！")
-	case len(ret) > 0:
-		info.Println("系统将监视以下类型的文件:", ret)
-	}
-
 	return ret
 }
 
-func getAppName(outputName, wd string) string {
-	if len(outputName) == 0 {
+func getAppName(outputName, wd string) (string, error) {
+	if outputName == "" {
 		outputName = filepath.Base(wd)
 	}
 	if runtime.GOOS == "windows" && !strings.HasSuffix(outputName, ".exe") {
 		outputName += ".exe"
 	}
+
+	// 没有分隔符，表示仅有一个文件名，需要加上 wd
 	if strings.IndexByte(outputName, '/') < 0 || strings.IndexByte(outputName, filepath.Separator) < 0 {
-		outputName = wd + string(filepath.Separator) + outputName
+		outputName = filepath.Join(wd, outputName)
 	}
 
 	// 转成绝对路径
 	outputName, err := filepath.Abs(outputName)
 	if err != nil {
-		erro.Println(err)
+		return "", err
 	}
 
-	info.Println("输出文件为:", outputName)
-
-	return outputName
+	return outputName, nil
 }
--- gobuild_test.go
+++ gobuild_test.go
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-package main
+package gobuild
 
 import (
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -23,24 +25,30 @@ func TestGetExts(t *testing.T) {
 func TestRecursivePath(t *testing.T) {
 	a := assert.New(t)
 
-	a.Equal(recursivePaths(false, []string{"./testdir"}), []string{
-		"./testdir",
-	})
+	paths, err := recursivePaths(false, []string{"./testdir"})
+	a.NotError(err).
+		Equal(paths, []string{
+			"./testdir",
+		})
 
-	a.Equal(recursivePaths(true, []string{"./testdir"}), []string{
+	paths, err = recursivePaths(true, []string{"./testdir"})
+	a.NotError(err).Equal(paths, []string{
 		"./testdir",
 		"testdir/testdir1",
 		"testdir/testdir2",
 		"testdir/testdir2/testdir3",
 	})
 
-	a.Equal(recursivePaths(true, []string{"./testdir/testdir1", "./testdir/testdir2"}), []string{
-		"./testdir/testdir1",
-		"./testdir/testdir2",
-		"testdir/testdir2/testdir3",
-	})
+	paths, err = recursivePaths(true, []string{"./testdir/testdir1", "./testdir/testdir2"})
+	a.NotError(err).
+		Equal(paths, []string{
+			"./testdir/testdir1",
+			"./testdir/testdir2",
+			"testdir/testdir2/testdir3",
+		})
 
-	a.Equal(recursivePaths(true, []string{"./testdir/testdir2"}), []string{
+	paths, err = recursivePaths(true, []string{"./testdir/testdir2"})
+	a.NotError(err).Equal(paths, []string{
 		"./testdir/testdir2",
 		"testdir/testdir2/testdir3",
 	})
@@ -52,4 +60,28 @@ func TestSplitArgs(t *testing.T) {
 	a.Equal(splitArgs("x=5 y=6"), []string{"x", "5", "y", "6"})
 	a.Equal(splitArgs("xxx=5 -yy=6 -bool"), []string{"xxx", "5", "-yy", "6", "-bool"})
 	a.Equal(splitArgs("xxx=5 yy=6 bool="), []string{"xxx", "5", "yy", "6", "bool"})
+}
+
+func TestGetAppName(t *testing.T) {
+	a := assert.New(t)
+
+	name, err := getAppName("", "./testdir")
+	a.NotError(err)
+	if runtime.GOOS != "windows" {
+		a.True(strings.HasSuffix(name, "testdir"), name)
+	} else {
+		a.True(strings.HasSuffix(name, "testdir.exe"), name)
+	}
+
+	name, err = getAppName("a", "./testdir")
+	a.NotError(err)
+	if runtime.GOOS != "windows" {
+		a.True(strings.HasSuffix(name, "testdir/a"), name)
+	} else {
+		a.True(strings.HasSuffix(name, "testdir/a.exe"), name)
+	}
+
+	name, err = getAppName("a.exe", "./testdir")
+	a.NotError(err).
+		True(strings.HasSuffix(name, "testdir/a.exe"), name)
 }
